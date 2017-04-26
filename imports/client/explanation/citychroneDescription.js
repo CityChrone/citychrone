1d757f3740b9057ee7b1c81ef56c082365cd4833
indaco <ocadni@gmail.com>
2017-04-26 17:56:54 +0200

    new

--- imports/client/explanation/citychroneDescription.js
+++ imports/client/explanation/citychroneDescription.js
@@ -2,16 +2,9 @@ import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 import { Router } from 'meteor/iron:router';
 import '/imports/client/explanation/citychroneDescription.html';
+import '/imports/client/selector/socialButtons.js';
 
 Template.citychroneDescription.helpers({
-	shareData: function() {
-    return { 
-    	title: 'CityChrone - visualizing city kwonledge.',
-    	url: 'http://citychrone.org',
-    	description : "sharing information"
-    	 }
-  }
-
 });
 
 Template.citychroneDescription.events({});
